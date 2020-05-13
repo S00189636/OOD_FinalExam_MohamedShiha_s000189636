9095c1ca9ee5908c8587a6ceb7842d2ff9330b83
mohamed shiha <46888852+S00189636@users.noreply.github.com>
2020-05-13 10:22:14 +0100

    Created a phone constructor

--- MohamadShiha_S00189636/MohamadShiha_S00189636/Phone.cs
+++ MohamadShiha_S00189636/MohamadShiha_S00189636/Phone.cs
@@ -8,12 +8,25 @@ namespace MohamadShiha_S00189636
 {
     public class Phone
     {
+
+        
+
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string OperatingSystem { get; set; }
         public string OS_Image { get; set; }
         public string Phone_Image { get; set; }
 
+        // used in test project
+        public Phone() { }
+        public Phone(string name,decimal price,string operatingSystem,string oS_Image,string phone_Image) 
+        {
+            Name = name;
+            Price = price;
+            OperatingSystem = operatingSystem;
+            OS_Image = oS_Image;
+            Phone_Image = phone_Image;
+        }
 
 
         public void IncreasePrice(decimal percentage)
