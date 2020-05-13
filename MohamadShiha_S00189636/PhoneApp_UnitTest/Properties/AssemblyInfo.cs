e21754dd4ce0e64f1026d89d0fd7ee182b941ab0
mohamed shiha <46888852+S00189636@users.noreply.github.com>
2020-05-13 10:16:21 +0100

    Unit test is done

---
+++ MohamadShiha_S00189636/PhoneApp_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PhoneApp_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhoneApp_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a1402fa-39a0-42d3-81af-1809037ceef2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MohamadShiha_S00189636/PhoneApp_UnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MohamadShiha_S00189636;
+namespace PhoneApp_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // this will make test if the phone priced at 100 will increase by 10 
+        //if i used the method giving 10 as a parameter
+        [TestMethod]
+        public void Test_Phone_IncreasePrice()
+        {
+            // Arrange 
+            Phone testPhone = new Phone()
+            {
+                Price = 100
+            };
+            decimal expected = 110;
+            // Act
+            testPhone.IncreasePrice(10);
+            decimal actual = testPhone.Price;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
